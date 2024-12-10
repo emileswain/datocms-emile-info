cc4daf1c44dde784d98871bcb1e634051e65d40a
Emile Swain <Emile.Swain@akqa.com>
2024-12-10 12:49:56 +0000

    Fixed bad component import breaking netlify build.

--- src/routes/components/[slug]/+page.server.ts
+++ src/routes/components/[slug]/+page.server.ts
@@ -1,10 +1,9 @@
-
-import { TagFragment } from '$lib/datocms/commonFragments';
-import { graphql } from '$lib/datocms/graphql';
-import { generateRealtimeSubscription } from '$lib/datocms/queries';
-import { error } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
-import {BlockLayoutFragment} from "../../../lib/components/BlockLayout/fragments";
+import {TagFragment} from '$lib/datocms/commonFragments';
+import {graphql} from '$lib/datocms/graphql';
+import {generateRealtimeSubscription} from '$lib/datocms/queries';
+import {error} from '@sveltejs/kit';
+import type {PageServerLoad} from './$types';
+import {BlockLayoutFragment} from '$lib/components/BlockLayout/fragments';
 
 /**
  * The GraphQL query that will be executed for this route to generate the page
@@ -13,22 +12,22 @@ import {BlockLayoutFragment} from "../../../lib/components/BlockLayout/fragments
  * Thanks to gql.tada, the result will be fully typed!
  */
 const query = graphql(
-  /* GraphQL */ `
-    query PageQuery($slug: String!) {
-      page(filter: { slug: { eq: $slug } }) {
-        _seoMetaTags {
-          ...TagFragment
+    /* GraphQL */ `
+        query PageQuery($slug: String!) {
+            page(filter: { slug: { eq: $slug } }) {
+                _seoMetaTags {
+                    ...TagFragment
+                }
+                title
+                shortDescription
+                _firstPublishedAt
+                content {
+                    ...BlockLayoutFragment
+                }
+            }
         }
-        title
-        shortDescription
-        _firstPublishedAt
-        content {
-          ...BlockLayoutFragment
-        }
-      }
-    }
-  `,
-  [TagFragment,  BlockLayoutFragment],
+    `,
+    [TagFragment, BlockLayoutFragment],
 );
 
 /**
@@ -37,14 +36,14 @@ const query = graphql(
  * active)
  */
 export const load: PageServerLoad = async (event) => {
-  // console.log(`PageServerLoad (event ) ${JSON.stringify(event, null, 4)}`);
-  const subscription = await generateRealtimeSubscription(event, query, {
-    slug: event.params.slug,
-  });
-  // console.log(`Load: ${subscription.initialData?.homepage?.title}}`)
-  if (!subscription.initialData?.page) {
-    error(404, 'Page not found');
-  }
+    // console.log(`PageServerLoad (event ) ${JSON.stringify(event, null, 4)}`);
+    const subscription = await generateRealtimeSubscription(event, query, {
+        slug: event.params.slug,
+    });
+    // console.log(`Load: ${subscription.initialData?.homepage?.title}}`)
+    if (!subscription.initialData?.page) {
+        error(404, 'Page not found');
+    }
 
-  return { subscription };
+    return {subscription};
 };
